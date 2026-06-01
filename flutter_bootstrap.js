4c27c3920aced50d580e5bd3015b6661535fbdc6
Kedar-27 <kedarjsukerkar@yahoo.co.in>
2026-06-01 18:04:12 +0000

    Update from https://github.com/Kedar-27/portfolio/commit/62222166dd6b774cf0793a7ebe8fecf432f185b0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -1,6 +1,6 @@
 // Generated file. Do not edit.
 
-const serviceWorkerVersion = '"3980482825" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */';
+const serviceWorkerVersion = '"4123656085"';
 
 const scriptTags = document.getElementsByTagName("script");
 const scriptTag = scriptTags[scriptTags.length - 1];
